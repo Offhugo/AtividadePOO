b821fa2e1ea9e3ac6761f8fac59f03afc67d13db
offhugo <hugo.mendes.313@gmail.com>
2024-03-29 21:53:02 -0300

    classe gerente foi aplicada

--- Executavel.java
+++ Executavel.java
@@ -3,10 +3,11 @@ package JavaCores.AtividadesClasses.AtvPraticas.ProvaPOO2;
 public class Executavel {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario(0, "", "", 0, "");
+        Gerente gerente = new Gerente(0, "", "", 0, "");
 
-        funcionario.cadastrar();
-
-        funcionario.consultar();
-
+        //funcionario.cadastrar();
+        //funcionario.consultar();
+        gerente.cadastrar();
+        gerente.aplicarAumento();
     }
 }
--- Gerente.java
+++ Gerente.java
@@ -1,4 +1,29 @@
 package JavaCores.AtividadesClasses.AtvPraticas.ProvaPOO2;
 
-public class Gerente {
+public class Gerente extends Funcionario {
+    //Atributos
+    private double porcetagemDoAumento;
+    //Construtor
+    public Gerente(int matricula, String nome, String status, double salario, String setor){
+        super(matricula, nome, status, salario, setor);
+    }
+    //Metodos especificos
+
+    public void aplicarAumento(){
+        if(getSalario() > 5000){
+            setPorcetagemDoAumento(1.35);
+            System.out.printf("Aumento: %.2f", (getSalario() * getPorcetagemDoAumento()));
+        } else {
+            setPorcetagemDoAumento(1.20);
+            System.out.printf("Aumento: %.2f", (getSalario() * getPorcetagemDoAumento()));
+        }
+    }
+    //Metodos get e set
+    public double getPorcetagemDoAumento() {
+        return porcetagemDoAumento;
+    }
+
+    public void setPorcetagemDoAumento(double porcetagemDoAumento) {
+        this.porcetagemDoAumento = porcetagemDoAumento;
+    }
 }
